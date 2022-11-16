2db1d1c5e7498ac9763e6cdeabd9827a12493307
Selvi <slvkesuma@gmail.com>
2022-11-16 22:58:45 +0200

    need to fix up header sub-notification next, update style on {time}

--- frontend/src/App-styles.js
+++ frontend/src/App-styles.js
@@ -1,37 +1,70 @@
 import styled, { css } from 'styled-components';
 
+// for Notification, NotifNumber and Mark all as read
 export const HeaderNotification = styled.div`
-margin: 10px 30px ;
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-`
-export const MainNotificationBox = styled.div`
-margin: 48px auto 48px auto;
-border-radius: 18px;
-height: 900px;
-width: 850px;
-background-color: #ffffff;
-`
-export const GeneralNotification = styled.div`
-background-color: #f6fafd;
-margin: 10px 30px ;
-border-radius: 18px;
-display: flex;
+	margin: 10px 30px ;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
 `
 
+// for the parent container of all notification (light blue)
+export const MainNotificationBox = styled.div`
+	margin: 48px auto 48px auto;
+	border-radius: 18px;
+	height: 900px;
+	width: 730px;
+	background-color: #ffffff;
+`
+
+// general settings for the container of each notifications; it contains three divs (or more), for example the AviNotification, GeneralNotificationContent, and sometimes PrivateMessageNotification & ImageNotification.
+export const GeneralNotification = styled.div`
+	background-color: #f6fafd;
+	margin: 10px 30px ;
+	border-radius: 18px;
+	display: flex;
+`
+
+// the div that contains the name & time. Might also contain PrivateMessageNotification and associated image, depending on the activity.
 export const GeneralNotificationContent = styled.div`
-margin: 18px;
+	margin: 18px 18px 18px 0px;
+	display:  ${props => props.attachedimage ? "flex" : "block"};
+	flex-direction: row;
+	align-items: flex-start;
+	justify-content: space-between;
+	width: 580px;
 `
+
+// display picture of each user.
 export const AviImage = styled.img`
-max-width: 100%;
+	max-width: 100%;
 `
+
+// the container of AviImage
 export const AviNotification = styled.div`
-width: 50px;
-margin-top: 18px;
-margin-bottom: 18px;
-margin-left: 18px;
+	width: 50px;
+	margin: 18px
 `
+
+// images attached to a post; e.g. in a commented post
+export const AttachedImage = styled.div`
+	height: fit-content;
+	width: 50px;
+	float: right;
+	margin-right: 24px;
+`
+
+// container of private message
 export const PrivateMessageNotification = styled.div`
-width: 650px;
+	margin-top: 12px;
+	padding: 16px;
+	color: #292d30;
+	width: 518px;
+	border: solid;
+	border-color: hsl(205, 33%, 90%);
+	border-width: 1px;
+`
+
+export const NameFormat = styled.span`
+	font-weight: 800;
 `
--- frontend/src/components/notif-activities.js
+++ frontend/src/components/notif-activities.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GeneralNotification, GeneralNotificationContent, AviNotification, AviImage, PrivateMessageNotification } from '../App-styles'
+import { GeneralNotification, GeneralNotificationContent, AviNotification, AviImage, PrivateMessageNotification, AttachedImage, NameFormat } from '../App-styles'
 
 
 export const Reacted = ({ name, target, time, avi }) => {
@@ -8,8 +8,11 @@ export const Reacted = ({ name, target, time, avi }) => {
 			<AviNotification>
 				<AviImage src={avi} alt="profile" ></AviImage>
 			</AviNotification>
+
 			<GeneralNotificationContent>
-				<div>{name} reacted to your recent post {target}</div>
+				<div>
+					<NameFormat>{name}</NameFormat> reacted to your recent post {target}
+				</div>
 				<div>{time}</div>
 			</GeneralNotificationContent>
 		</GeneralNotification>
@@ -22,8 +25,11 @@ export const Followed = ({ name, target, time, avi }) => {
 			<AviNotification>
 				<AviImage src={avi} alt="profile"></AviImage>
 			</AviNotification>
+
 			<GeneralNotificationContent>
-				<div>{name} followed {target}</div>
+				<div>
+					<NameFormat>{name}</NameFormat> followed {target}
+				</div>
 				<div>{time}</div>
 			</GeneralNotificationContent>
 		</GeneralNotification>
@@ -36,8 +42,11 @@ export const JoinedGroup = ({ name, target, time, avi }) => {
 			<AviNotification>
 				<AviImage src={avi} alt="profile"></AviImage>
 			</AviNotification>
+
 			<GeneralNotificationContent>
-				<div>{name} has joined your group {target}</div>
+				<div>
+					<NameFormat>{name}</NameFormat> has joined your group {target}
+				</div>
 				<div>{time}</div>
 			</GeneralNotificationContent>
 		</GeneralNotification>
@@ -47,14 +56,21 @@ export const JoinedGroup = ({ name, target, time, avi }) => {
 export const PrivateMessage = ({ name, target, time, message, avi }) => {
 	return (
 		<GeneralNotification>
+
 			<AviNotification>
 				<AviImage src={avi} alt="profile"></AviImage>
 			</AviNotification>
+
 			<GeneralNotificationContent>
-				<div>{name} sent {target} a private message</div>
+				<div>
+					<NameFormat>{name}</NameFormat> sent {target} a private message
+				</div>
 				<div>{time}</div>
-				<PrivateMessageNotification>{message}</PrivateMessageNotification>
+				<PrivateMessageNotification>
+					{message}
+				</PrivateMessageNotification>
 			</GeneralNotificationContent>
+
 		</GeneralNotification>
 	)
 }
@@ -62,14 +78,23 @@ export const PrivateMessage = ({ name, target, time, message, avi }) => {
 export const Commented = ({ name, target, time, img, avi }) => {
 	return (
 		<GeneralNotification>
+
 			<AviNotification>
 				<AviImage src={avi} alt="profile"></AviImage>
 			</AviNotification>
-			<GeneralNotificationContent>
-				<div>{name} commented on {target}</div>
-				<div>{time}</div>
-				<div><img src={img} alt=""></img></div>
+
+			<GeneralNotificationContent attachedimage>
+				<div>
+					<div>
+						<NameFormat>{name}</NameFormat> commented on {target}
+					</div>
+					<div>{time}</div>
+				</div>
+				<AttachedImage>
+					<AviImage src={img} alt=""></AviImage>
+				</AttachedImage>
 			</GeneralNotificationContent >
+
 		</GeneralNotification>
 	)
 }
@@ -77,13 +102,18 @@ export const Commented = ({ name, target, time, img, avi }) => {
 export const LeftGroup = ({ name, target, time, img, avi }) => {
 	return (
 		<GeneralNotification>
+
 			<AviNotification>
 				<AviImage src={avi} alt="profile"></AviImage>
 			</AviNotification>
+
 			<GeneralNotificationContent>
-				<div>{name} left the group {target}</div>
+				<div>
+					<NameFormat>{name}</NameFormat> left the group {target}
+				</div>
 				<div>{time}</div>
 			</GeneralNotificationContent>
+
 		</GeneralNotification>
 	)
 }
@@ -99,7 +129,7 @@ const components = {
 
 export const NotificationList = ({ data }) => {
 	let MyComponent = components[data.activity]
-
+	// pass useState for bgcolor?
 	return (
 		<div>
 			<MyComponent name={data.name} target={data.target} time={data.time} message={data.message} img={data.img} avi={data.avi} />
